8e84c689553a51737f446321166b1fecf5a1d835
Vitor Dino <vitor@kunstdesign.com.br>
2019-02-01 14:29:39 -0200

    :sparkles: add Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, lazy, Suspense } from 'react'
 import { Router, Location } from '@reach/router'
 import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
 import Loader from 'components/Loader'
 const SearchView = lazy(() => import('components/SearchView'))
 const FavoritesView = lazy(() => import('components/FavoritesView'))
@@ -9,17 +10,20 @@ const DetailView = lazy(() => import('components/DetailView'))
 const App = () => (
 	<Fragment>
 		<Navbar/>
-		<Location>
-			{({location}) => (
-				<Suspense fallback={<Loader/>}>
-					<Router location={location}>
-						<SearchView path='/'/>
-						<FavoritesView path='/favorites'/>
-						<DetailView path='/:movieId'/>
-					</Router>
-				</Suspense>
-			)}
-		</Location>
+		<div style={{flex: 1}}>
+			<Location>
+				{({location}) => (
+					<Suspense fallback={<Loader/>}>
+						<Router location={location}>
+							<SearchView path='/'/>
+							<FavoritesView path='/favorites'/>
+							<DetailView path='/:movieId'/>
+						</Router>
+					</Suspense>
+				)}
+			</Location>
+		</div>
+		<Footer/>
 	</Fragment>
 )
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import Container from 'components/Container'
+
+const Wrapper = styled.div`
+	padding: 1.5rem 0;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: space-between;
+`
+
+const Anchor = styled.a`
+	display: block;
+	text-decoration: none;
+	color: currentColor;
+	margin: -0.5rem;
+	padding: 0.5rem;
+	border-radius: 0.125rem;
+	font-weight: 300;
+	color: ${p => p.theme.colors.grey};
+	&:hover, &:focus {color: ${p => p.theme.colors.lightGrey}}
+	&:focus{${p => p.theme.focusShadow}}
+	& strong{
+		font-weight: 500;
+	}
+`
+
+
+const Footer = () => (
+	<Container>
+		<Wrapper>
+			<Anchor href='https://significa.pt'>design: <strong>significa</strong></Anchor>
+			<Anchor href='https://vitordino.com'>dev: <strong>vitordino</strong></Anchor>
+		</Wrapper>
+	</Container>
+)
+
+export default Footer
