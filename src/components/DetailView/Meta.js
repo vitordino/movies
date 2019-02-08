ac4d957f4c1019e28c3c462ffef3268a9105ee14
Vitor Dino <vitor@kunstdesign.com.br>
2019-02-08 03:41:11 -0200

    :art: DetailView refactor

--- src/components/DetailView/Meta.js
+++ src/components/DetailView/Meta.js
@@ -7,10 +7,29 @@ const getRating = (release_dates = {}) => (
 	release_dates?.results?.find(x => x.iso_3166_1 === 'US')?.release_dates[0]?.certification || null
 )
 
+const getFirst = ({release_date, first_air_date, birthday}) => {
+	return (first_air_date || birthday || release_date)?.split('-')[0]
+}
+
+const getSecond = ({runtime, last_air_date, in_production}) => {
+	if(runtime) return `${runtime} min`
+	if(in_production) return 'Present'
+	if(last_air_date) return last_air_date.split('-')[0]
+	return null
+}
+
+const getHighlight = ({release_dates, number_of_seasons}) => {
+	if(release_dates) return getRating(release_dates)
+	if(number_of_seasons) return `${number_of_seasons} seasons`
+	return null
+}
+
 const Wrapper = styled.div`
 	display: flex;
 	align-items: center;
 `
+
+const SeparatedText = styled(Text)`span+span{&:before{content: ' • '}}`
 
 const Rating = styled(Text)`
 	background: ${p => p.theme.colors.lightGrey};
@@ -30,15 +49,14 @@ const Rating = styled(Text)`
 	`}
 `
 
-const Meta = ({year, runtime, release_date, release_dates}) => (
+const Meta = data => (
 	<Wrapper style={{margin: '1.5rem 0'}}>
-		<Text sm={1} color={p => p.theme.colors.lightGrey || ''}>
-			{runtime && `${runtime} min`}
-			{runtime && year && ` • `}
-			{year && `${year}`}
-			{year && getRating(release_dates) && ` • `}
-		</Text>
-		{getRating(release_dates) && <Rating>{getRating(release_dates)}</Rating>}
+		<SeparatedText sm={1} color={p => p.theme.colors.lightGrey || ''}>
+			{getFirst(data) && <span>{getFirst(data)}</span>}
+			{getSecond(data) && getSecond(data) !== getFirst(data) && <span>{getSecond(data)}</span>}
+			{(getFirst(data) || getSecond(data)) && getHighlight(data) && <span/>}
+		</SeparatedText>
+		{getHighlight(data) && <Rating>{getHighlight(data)}</Rating>}
 	</Wrapper>
 )
 
